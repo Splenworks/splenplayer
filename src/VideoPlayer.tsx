b7ae0a17b997ba5d071460a24b6ebe356126269f
Royce Joe <roylory@gmail.com>
2024-03-13 19:46:08 -0700

    show controls

--- src/VideoPlayer.tsx
+++ src/VideoPlayer.tsx
@@ -8,12 +8,15 @@ const VideoPlayer: React.FC<VideoPlayerProps> = ({ videoFile }) => {
   const videoSrc = URL.createObjectURL(videoFile)
 
   return (
-    <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center">
+    <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black">
       <video
         src={videoSrc}
         className="object-contain max-w-full max-h-full min-w-full min-h-full"
         autoPlay
-      />
+        controls
+      >
+        <source src={videoSrc} type={videoFile.type} />
+      </video>
     </div>
   )
 }
