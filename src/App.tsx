ed480b63240444a76d921b7eeccf677c4cb77097
Royce Joe <roylory@gmail.com>
2024-03-13 18:35:06 -0700

    copy scaffolding from splenview

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import DragDropArea from "./DragDropArea"
+import Footer from "./Footer"
+
+function App() {
+  return (
+    <>
+      <DragDropArea />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ src/DragDropArea.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react"
+import { twJoin } from "tailwind-merge"
+import Spinner from "./Spinner"
+
+interface DragDropAreaProps {}
+
+const DragDropArea: React.FC<DragDropAreaProps> = () => {
+  const [dragging, setDragging] = useState(false)
+
+  const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setDragging(true)
+  }
+
+  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setDragging(false)
+  }
+
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+  }
+
+  const handleDrop = async (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault()
+    setDragging(false)
+  }
+
+  return (
+    <div className="fixed top-0 left-0 right-0 bottom-4">
+      <div
+        className={twJoin(
+          "absolute inset-10 rounded-xl border-dashed border-4 border-gray-300 cursor-pointer flex flex-col items-center justify-center transition-colors duration-300 ease-in-out",
+          dragging && "border-blue-500 bg-blue-100",
+        )}
+        onDragEnter={handleDragEnter}
+        onDragLeave={handleDragLeave}
+        onDragOver={handleDragOver}
+        onDrop={handleDrop}
+      >
+        {dragging ? (
+          <p className="text-xl font-semibold px-4 text-center">Drop here</p>
+        ) : (
+          <>
+            <p className="text-xl font-semibold px-4 text-center">
+              Drag and drop a video file here
+            </p>
+          </>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default DragDropArea
