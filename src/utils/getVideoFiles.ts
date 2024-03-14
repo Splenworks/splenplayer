6dbd5dc394288d7f25d7a411712e37459e366c71
Royce Joe <roylory@gmail.com>
2024-03-13 21:39:38 -0700

    filter by video file extension

--- src/utils/getVideoFiles.ts
+++ src/utils/getVideoFiles.ts
@@ -1,5 +1,16 @@
+const looksLikeVideo = (name: string) => {
+  const lowerCasedName = name.toLowerCase()
+  return (
+    lowerCasedName.endsWith(".mp4") ||
+    lowerCasedName.endsWith(".webm") ||
+    lowerCasedName.endsWith(".mov") ||
+    lowerCasedName.endsWith(".avi") ||
+    lowerCasedName.endsWith(".mkv")
+  )
+}
+
 export const getVideoFiles = (files: File[]) => {
   return files
-    .filter((file) => file.type.startsWith("video/"))
+    .filter((file) => looksLikeVideo(file.name))
     .sort((a, b) => a.name.localeCompare(b.name))
 }
