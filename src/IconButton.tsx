ce729a5ad1cc3b4ed4912c44d0ee7730711f6da7
Royce Joe <roylory@gmail.com>
2024-03-15 20:29:42 -0700

    icon button comp

---
+++ src/IconButton.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { twMerge } from "tailwind-merge"
+
+interface IconButtonProps {
+  svgIcon: React.FunctionComponent
+  className?: string
+  onClick?: () => void
+}
+
+const IconButton: React.FC<IconButtonProps> = ({
+  svgIcon,
+  className,
+  onClick,
+}) => {
+  return (
+    <button
+      className={twMerge(
+        "w-8 h-8 flex justify-center items-center hover:bg-zinc-500 hover:bg-opacity-50 rounded-full transition-colors duration-300 ease-in-out",
+        className,
+      )}
+      onClick={onClick}
+    >
+      {svgIcon({ className: "w-6 h-6 text-white" })}
+    </button>
+  )
+}
+
+export default IconButton
