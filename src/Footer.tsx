65f0cf2d25046595031a1456a8c67db59775de97
Royce Joe <roylory@gmail.com>
2024-03-28 22:40:01 -0700

    dark mode button in the footer instead

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -5,11 +5,11 @@ import { MoonIcon, SunIcon } from "@heroicons/react/16/solid"
 const Footer: React.FC = () => {
   return (
     <footer className="absolute bottom-0 left-0 right-0 bg-white dark:bg-neutral-900">
-      <div className="flex items-center justify-between h-16 px-16">
+      <div className="flex relative items-center justify-center h-16 mx-8 md:mx-16">
         <p className="text-black dark:text-white text-sm">
           Version {APP_VERSION}.{CommitHash.substring(0, 7)}
         </p>
-        <div className="flex items-center justify-end">
+        <div className="absolute right-0">
           <span
             onClick={() => {
               document.documentElement.classList.toggle("dark")
