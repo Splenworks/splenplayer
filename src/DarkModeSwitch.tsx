d9a44adf79ff06872da3c6a9cf5a57800e44ecac
Royce Joe <roylory@gmail.com>
2024-03-30 12:27:05 -0700

    darkmode switch

--- src/DarkModeSwitch.tsx
+++ src/DarkModeSwitch.tsx
@@ -1,2 +1,46 @@
-// https://www.npmjs.com/package/react-toggle-dark-mode
-// https://www.geeksforgeeks.org/how-to-create-a-toggle-switch-in-react-as-a-reusable-component/
+import { DarkModeSwitch as DarkModeSwitchIcon } from "react-toggle-dark-mode"
+import "./DarkModeSwitch.pcss"
+import { useState } from "react"
+
+const DarkModeSwitch = () => {
+  const [darkMode, setDarkMode] = useState(
+    document.documentElement.classList.contains("dark"),
+  )
+  return (
+    <div className="darkmode-switch">
+      <input
+        type="checkbox"
+        className="checkbox"
+        name="darkModeSwitch"
+        id="darkModeSwitch"
+        checked={darkMode}
+        onChange={() => {
+          setDarkMode((darkMode) => !darkMode)
+          document.documentElement.classList.toggle("dark")
+          localStorage.setItem(
+            "theme",
+            document.documentElement.classList.contains("dark")
+              ? "dark"
+              : "light",
+          )
+        }}
+      />
+      <label className="label" htmlFor="darkModeSwitch">
+        <span className="inner" />
+        <span className="switch">
+          <DarkModeSwitchIcon
+            size={16}
+            checked={darkMode}
+            sunColor="black"
+            moonColor="black"
+            style={{ margin: "4px" }}
+            onChange={() => {}}
+            className="text-black"
+          />
+        </span>
+      </label>
+    </div>
+  )
+}
+
+export default DarkModeSwitch
