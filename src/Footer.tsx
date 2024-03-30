90c489c3c3457de08e9e5a7611c50fe5ffcf8f3f
Royce Joe <roylory@gmail.com>
2024-03-30 12:30:48 -0700

    Remove unnecessary relative class from footer div

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,7 +4,7 @@ import CommitHash from "virtual:commit-hash"
 const Footer: React.FC = () => {
   return (
     <footer className="absolute bottom-0 left-0 right-0 bg-white dark:bg-neutral-900">
-      <div className="flex relative items-center justify-center h-16 mx-8 md:mx-16">
+      <div className="flex items-center justify-center h-16 mx-8 md:mx-16">
         <p className="text-black dark:text-white text-sm">
           Version {APP_VERSION}.{CommitHash.substring(0, 7)}
         </p>
