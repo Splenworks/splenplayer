46b52017eb0d162df8267c5fbd0ecdc2d012d012
Royce Joe <roylory@gmail.com>
2024-05-11 15:07:20 -0700

    never store your data

--- src/DragDropArea.tsx
+++ src/DragDropArea.tsx
@@ -77,7 +77,7 @@ const DragDropArea: React.FC<DragDropAreaProps> = ({ setMediaFiles }) => {
           ref={fileInputRef}
           onChange={handleFileInputChange}
         />
-        <div className="px-4 pb-4 text-black dark:text-white pointer-events-none">
+        <div className="px-4 pb-12 text-black dark:text-white pointer-events-none">
           {dragging ? (
             <p className="text-xl font-bold text-center text-gray-50 dark:text-white shadow-gray-600 dark:shadow-black [text-shadow:_0_5px_5px_var(--tw-shadow-color,0.5)]">
               <Trans i18nKey="dragDropArea.dropHere" />
@@ -85,7 +85,7 @@ const DragDropArea: React.FC<DragDropAreaProps> = ({ setMediaFiles }) => {
           ) : (
             <div className="flex flex-col items-center justify-center">
               <PlayCircleIcon className="mb-8 w-24 h-24 text-pink-900 dark:text-pink-700" />
-              <p className="mb-4 text-xl font-bold text-center">
+              <p className="mb-4 text-xl text-center font-bold">
                 <Trans
                   i18nKey="dragDropArea.mainMessage"
                   components={{
@@ -93,13 +93,16 @@ const DragDropArea: React.FC<DragDropAreaProps> = ({ setMediaFiles }) => {
                   }}
                 />
               </p>
-              <p className="text-lg text-center">
+              <p className="mb-4 text-lg text-center font-semibold">
                 <Trans
                   i18nKey="dragDropArea.subMessage"
                   components={{
                     u: <span className="text-pink-900 dark:text-pink-700" />,
                   }}
                 />
+              </p>
+              <p className="text-center">
+                <Trans i18nKey="dragDropArea.neverStoreYourData" />
               </p>
             </div>
           )}
