95937224cab9f7d1f3a57a3ae8471841ba376a9a
Royce Joe <roylory@gmail.com>
2024-06-01 15:00:24 -0700

    footer text color

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -20,7 +20,7 @@ const Footer: React.FC = () => {
     <footer className="absolute bottom-0 left-0 right-0 bg-white dark:bg-neutral-900">
       <div className="flex items-center justify-center h-16 mx-8 md:mx-16">
         <div className="flex-1 hidden sm:block"></div>
-        <p className="text-black dark:text-white text-sm">
+        <p className="text-gray-800 dark:text-gray-300 text-sm">
           &copy; 2024 SplenWork Inc. All rights reserved.
           {/* <Trans
             i18nKey="footer.checkOutProduct"
