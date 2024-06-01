9e2a280c10d590dd6d69fecc69dfeb28cfda9f65
Royce Joe <roylory@gmail.com>
2024-06-01 14:53:10 -0700

    copyright in footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,27 +1,28 @@
 import React from "react"
-import { Trans } from "react-i18next"
+// import { Trans } from "react-i18next"
 import CommitHash from "virtual:commit-hash"
 
-const otherProducts = [
-  {
-    name: "SplenView",
-    url: "https://splenview.com",
-  },
-  {
-    name: "txtpad.io",
-    url: "https://txtpad.io",
-  },
-]
+// const otherProducts = [
+//   {
+//     name: "SplenView",
+//     url: "https://splenview.com",
+//   },
+//   {
+//     name: "txtpad.io",
+//     url: "https://txtpad.io",
+//   },
+// ]
 
 const Footer: React.FC = () => {
-  const randomProduct =
-    otherProducts[Math.floor(Math.random() * otherProducts.length)]
+  // const randomProduct =
+  //   otherProducts[Math.floor(Math.random() * otherProducts.length)]
   return (
     <footer className="absolute bottom-0 left-0 right-0 bg-white dark:bg-neutral-900">
       <div className="flex items-center justify-center h-16 mx-8 md:mx-16">
         <div className="flex-1 hidden sm:block"></div>
         <p className="text-black dark:text-white text-sm">
-          <Trans
+          &copy; 2024 SplenWork Inc. All rights reserved.
+          {/* <Trans
             i18nKey="footer.checkOutProduct"
             components={{
               link: (
@@ -34,7 +35,7 @@ const Footer: React.FC = () => {
                 </a>
               ),
             }}
-          />
+          /> */}
         </p>
         <div className="flex-1 text-sm text-right text-white dark:text-neutral-900 hidden sm:block">
           Version {APP_VERSION}.{CommitHash.substring(0, 7)}
