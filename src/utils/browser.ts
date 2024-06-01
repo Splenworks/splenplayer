7ec563a1f8ca902b5d1c833a3cd263cd44145e2e
Royce Joe <roylory@gmail.com>
2024-06-01 13:58:47 -0700

    refactor: utils

--- src/utils/browser.ts
+++ src/utils/browser.ts
@@ -1,3 +1,7 @@
+export const isSafari = /^((?!chrome|android).)*safari/i.test(
+  navigator.userAgent,
+)
+
 export const isMac = (() => {
   // https://developer.mozilla.org/en-US/docs/Web/API/NavigatorID/userAgent#value
   const platformRegex = /\(.*\)/
