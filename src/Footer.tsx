8a34ae18766e838a8286f29fbfac407901ef9ba6
Royce Joe <roylory@gmail.com>
2024-07-03 14:53:32 -0700

    revert footer to show other products

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,29 +1,27 @@
 import React from "react"
-// import { Trans } from "react-i18next"
+import { Trans } from "react-i18next"
 import CommitHash from "virtual:commit-hash"
 
-// const otherProducts = [
-//   {
-//     name: "SplenView",
-//     url: "https://splenview.com",
-//   },
-//   {
-//     name: "txtpad.io",
-//     url: "https://txtpad.io",
-//   },
-// ]
+const otherProducts = [
+  {
+    name: "SplenView",
+    url: "https://splenview.com",
+  },
+  {
+    name: "txtpad.io",
+    url: "https://txtpad.io",
+  },
+]
 
 const Footer: React.FC = () => {
-  // const randomProduct =
-  //   otherProducts[Math.floor(Math.random() * otherProducts.length)]
-  const currentYear = new Date().getFullYear()
+  const randomProduct =
+    otherProducts[Math.floor(Math.random() * otherProducts.length)]
   return (
     <footer className="absolute bottom-0 left-0 right-0 bg-white dark:bg-neutral-900">
       <div className="flex items-center justify-center h-16 mx-8 md:mx-16">
         <div className="flex-1 hidden sm:block"></div>
         <p className="text-gray-800 dark:text-gray-300 text-sm">
-          &copy; {currentYear} SplenWork Inc. All rights reserved.
-          {/* <Trans
+          <Trans
             i18nKey="footer.checkOutProduct"
             components={{
               link: (
@@ -36,7 +34,7 @@ const Footer: React.FC = () => {
                 </a>
               ),
             }}
-          /> */}
+          />
         </p>
         <div className="flex-1 text-sm text-right text-white dark:text-neutral-900 hidden sm:block">
           Version {APP_VERSION}.{CommitHash.substring(0, 7)}
