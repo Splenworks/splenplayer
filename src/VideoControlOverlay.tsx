0a8a8542c6c362629f787c4f95b639122f22e2d6
Royce Joe <roylory@gmail.com>
2024-12-15 13:33:29 -0800

    handle multiple lines in subtitle

--- src/VideoControlOverlay.tsx
+++ src/VideoControlOverlay.tsx
@@ -341,12 +341,19 @@ const VideoControlOverlay: React.FC<VideoControlOverlayProps> = ({
         className={twJoin("absolute inset-0", isAudio ? "flex" : "hidden")}
       />
       {showSubtitle && subtitles.current.length > 0 && (
-        <p
-          className="absolute left-4 right-4 flex h-10 items-center justify-center text-center font-sans font-semibold text-white sm:text-xl md:text-2xl lg:text-3xl"
+        <div
+          className="absolute left-4 right-4 flex flex-col items-center justify-center text-center font-sans font-semibold text-white sm:text-xl md:text-2xl lg:text-3xl"
           style={{ textShadow: "0 0 8px black", bottom: captionBottomPosition }}
         >
-          {currentSubtitle}
-        </p>
+          {currentSubtitle
+            .split("\n")
+            .filter((line) => line.trim())
+            .map((line, index) => (
+              <p key={index} className="mb-1">
+                {line.trim()}
+              </p>
+            ))}
+        </div>
       )}
       <div
         className={twJoin(
