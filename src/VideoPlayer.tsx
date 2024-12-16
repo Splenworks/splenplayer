96e72c4fce1411bf27abc1e35e528d753a17cf11
Royce Joe <roylory@gmail.com>
2024-12-15 17:18:17 -0800

    Revert "remove forwardRef as it's not required in React 19 anymore"
    This reverts commit 1d2cc4656019043ceaf9f82cb31bea6393285ed2.

--- src/VideoPlayer.tsx
+++ src/VideoPlayer.tsx
@@ -1,31 +1,29 @@
+import { forwardRef } from "react"
 import { MediaFile } from "./utils/getMediaFiles"
 
 interface VideoPlayerProps {
   mediaFiles: MediaFile[]
   currentIndex: number
-  videoRef: React.RefObject<HTMLVideoElement>
 }
 
-const VideoPlayer: React.FC<VideoPlayerProps> = ({
-  mediaFiles,
-  currentIndex,
-  videoRef,
-}) => {
-  const blob = new Blob([mediaFiles[currentIndex].file], {
-    type:
-      mediaFiles[currentIndex].type === "audio" ? "audio/mpeg" : "video/mp4",
-  })
-  const videoSrc = URL.createObjectURL(blob)
+const VideoPlayer = forwardRef<HTMLVideoElement, VideoPlayerProps>(
+  ({ mediaFiles, currentIndex }, videoRef) => {
+    const blob = new Blob([mediaFiles[currentIndex].file], {
+      type:
+        mediaFiles[currentIndex].type === "audio" ? "audio/mpeg" : "video/mp4",
+    })
+    const videoSrc = URL.createObjectURL(blob)
 
-  return (
-    <div className="fixed bottom-0 left-0 right-0 top-0 flex items-center justify-center bg-black">
-      <video
-        ref={videoRef}
-        className="max-h-full min-h-full min-w-full max-w-full"
-        src={videoSrc}
-      />
-    </div>
-  )
-}
+    return (
+      <div className="fixed bottom-0 left-0 right-0 top-0 flex items-center justify-center bg-black">
+        <video
+          ref={videoRef}
+          className="max-h-full min-h-full min-w-full max-w-full"
+          src={videoSrc}
+        />
+      </div>
+    )
+  },
+)
 
 export default VideoPlayer
