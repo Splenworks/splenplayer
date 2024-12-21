7551b445e477219baa3c0a62350cae1104da687e
Royce Joe <roylory@gmail.com>
2024-12-20 22:41:20 -0800

    Refactor video control to use ProgressBar component for seeking functionality

--- src/VideoControlOverlay.tsx
+++ src/VideoControlOverlay.tsx
@@ -19,6 +19,7 @@ import {
 import { replaceBasicHtmlEntities } from "./utils/html"
 
 import IconButton from "./IconButton"
+import ProgressBar from "./ProgressBar"
 import ExitFullscreenIcon from "./assets/compress.svg?react"
 import FullscreenIcon from "./assets/expand.svg?react"
 import NextIcon from "./assets/next.svg?react"
@@ -545,23 +546,7 @@ const VideoControlOverlay: React.FC<VideoControlOverlayProps> = ({
           </div>
         </div>
         <div className="absolute bottom-2 left-2 right-2 mx-4 flex h-8 items-center justify-center">
-          <input
-            autoFocus
-            className={twJoin(
-              isSafari
-                ? "h-2 w-full cursor-pointer appearance-none rounded-full border border-neutral-500 bg-transparent accent-white outline-none [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white"
-                : "w-full cursor-pointer accent-white outline-none",
-            )}
-            type="range"
-            min="0"
-            max="100"
-            step="0.1"
-            onChange={handleSeek}
-            onKeyDown={(e) => {
-              e.preventDefault()
-            }}
-            value={seekValue}
-          />
+          <ProgressBar handleSeek={handleSeek} seekValue={seekValue} />
         </div>
       </div>
     </div>
