fcbc303ed5dc547c7670c70f818a1282d3135e8c
Royce Joe <roylory@gmail.com>
2025-07-17 15:20:14 -0700

    Revert "fix the issue in dev mode"
    This reverts commit ad582cf1ebc5f62e04fdf5f1de6a229596c6a4cc.

--- src/VideoPlayer.tsx
+++ src/VideoPlayer.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, useEffect, useMemo, useRef } from "react"
+import { forwardRef, useEffect, useMemo } from "react"
 import { MediaFile } from "./utils/getMediaFiles"
 
 interface VideoPlayerProps {
@@ -8,30 +8,19 @@ interface VideoPlayerProps {
 
 const VideoPlayer = forwardRef<HTMLVideoElement, VideoPlayerProps>(
   ({ mediaFiles, currentIndex }, videoRef) => {
-    const urlRef = useRef<string | null>(null)
-
     const videoSrc = useMemo(() => {
-      if (urlRef.current) {
-        URL.revokeObjectURL(urlRef.current)
-      }
-
       const blob = new Blob([mediaFiles[currentIndex].file], {
         type:
           mediaFiles[currentIndex].type === "audio" ? "audio/mpeg" : "video/mp4",
       })
-      const newUrl = URL.createObjectURL(blob)
-      urlRef.current = newUrl
-      return newUrl
+      return URL.createObjectURL(blob)
     }, [mediaFiles, currentIndex])
 
     useEffect(() => {
       return () => {
-        if (urlRef.current) {
-          URL.revokeObjectURL(urlRef.current)
-          urlRef.current = null
-        }
+        URL.revokeObjectURL(videoSrc)
       }
-    }, [])
+    }, [videoSrc])
 
     return (
       <div className="fixed bottom-0 left-0 right-0 top-0 flex items-center justify-center bg-black">
