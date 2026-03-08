28d7b98811279680706e757691f9e7a117646f32
Royce Joe <roylory@gmail.com>
2026-03-08 16:29:03 -0700

    gradient bg button style

--- src/DirectMediaUrlForm.tsx
+++ src/DirectMediaUrlForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { useTranslation } from "react-i18next"
+import { twJoin } from "tailwind-merge"
 import type { MediaFile } from "./types/MediaFiles"
 import { createUrlMediaFile } from "./utils/getMediaFiles"
 
@@ -42,11 +43,14 @@ const DirectMediaUrlForm: React.FC<DirectMediaUrlFormProps> = ({ setMedia }) => 
           onChange={(event) => {
             setMediaUrl(event.target.value)
           }}
-          className="min-w-0 flex-1 rounded-md border border-gray-300/60 px-3 py-2 text-sm text-black outline-hidden transition placeholder:text-gray-400 focus:border-pink-700 dark:border-white/25 dark:text-white dark:placeholder:text-gray-500 dark:focus:border-pink-500 bg-white dark:bg-neutral-800"
+          className="min-w-0 flex-1 rounded-md border border-gray-300/60 px-3 py-2 text-sm text-black outline-hidden transition placeholder:text-gray-400 focus:border-pink-700 dark:border-white/25 dark:text-white dark:placeholder:text-gray-500 dark:focus:border-pink-500 bg-white dark:bg-neutral-900"
         />
         <button
           type="submit"
-          className="cursor-pointer rounded-md bg-pink-800 px-4 py-2 text-sm font-semibold text-white transition hover:bg-pink-700 dark:bg-pink-700 dark:hover:bg-pink-600"
+          className={twJoin(
+            "cursor-pointer rounded-md px-4 py-2 text-sm font-semibold text-white transition",
+            "bg-gradient-to-r from-pink-700 to-pink-900 dark:from-pink-700 dark:to-pink-500",
+          )}
         >
           {t("dragDropArea.urlSubmit")}
         </button>
